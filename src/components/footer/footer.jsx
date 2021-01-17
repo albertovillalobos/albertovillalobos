90e819d465fc98d44718f5922348d6d3b49b82a9
Isaac <isaac@senpaibook.local>
2021-01-17 07:50:25 -0800

    update

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,9 +3,8 @@ import { footer, link } from './footer.module.scss';
 
 const year = new Date().getFullYear();
 
-const Footer = () => <div className={footer}>
-  Alberto Villalobos {year} &copy;
-  | <a className={link} href="https://github.com/albertovillalobos/albertovillalobos">Source</a>
-</div>
+const Footer = () => (
+  <div className={footer}>Alberto Villalobos {year} &copy;</div>
+);
 
 export default Footer;
