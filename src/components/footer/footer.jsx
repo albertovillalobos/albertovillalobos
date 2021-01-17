f859ce0315b6451420757c8bf6442ae746ae5825
Alberto <albertovillalobos@users.noreply.github.com>
2021-01-17 07:54:40 -0800

    update

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { footer, link } from './footer.module.scss';
+import { footer } from './footer.module.scss';
 
 const year = new Date().getFullYear();
 
-const Footer = () => <div className={footer}>
-  Alberto Villalobos {year} &copy;
-  | <a className={link} href="https://github.com/albertovillalobos/albertovillalobos">Source</a>
-</div>
+const Footer = () => (
+  <div className={footer}>Alberto Villalobos {year} &copy;</div>
+);
 
 export default Footer;
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -3,7 +3,6 @@ import { socialData, aboutMe1, aboutMe2, particularSkills } from './data.js';
 import styles, {
   container,
   presentation,
-  selfie,
   name,
   links,
   about,
@@ -17,39 +16,44 @@ import styles, {
 } from './home.module.scss';
 
 const Social = ({ href, className, fa }) => (
-  <a href={href} key={className} target="_blank" rel="noopener noreferrer" className={styles[className]}>
+  <a
+    href={href}
+    key={className}
+    target="_blank"
+    rel="noopener noreferrer"
+    className={styles[className]}
+  >
     <i className={fa} />
   </a>
-)
+);
 
-const skillColumn = ({ name, skills}) => (
+const skillColumn = ({ name, skills }) => (
   <div className={column} key={name}>
     <div className={skillTitle}>{name}</div>
     <ul className={skillList}>
-      {skills.map(s => <li key={s}>{s}</li> )}
+      {skills.map((s) => (
+        <li key={s}>{s}</li>
+      ))}
     </ul>
   </div>
-)
+);
 
-const Home = () => <div className={container}>
-  <div className={presentation}>
-    <img alt="alberto's selfie" className={selfie} src="https://i.imgur.com/rpkkoRr.jpg" />
-    <div className={name}>Alberto Villalobos</div>
-    <div className={links}>
-      {socialData.map(Social)}
+const Home = () => (
+  <div className={container}>
+    <div className={presentation}>
+      <div className={name}>Alberto Villalobos</div>
+      <div className={links}>{socialData.map(Social)}</div>
+    </div>
+    <div className={about}>
+      <div className={title}>About</div>
+      <div className={copy}>{aboutMe1}</div>
+      <div className={copy}>{aboutMe2}</div>
+    </div>
+    <div className={skillset}>
+      <div className={title}>Skills</div>
+      <div className={skillColumns}>{particularSkills.map(skillColumn)}</div>
     </div>
   </div>
-  <div className={about}>
-    <div className={title}>About</div>
-    <div className={copy}>{aboutMe1}</div>
-    <div className={copy}>{aboutMe2}</div>
-  </div>
-  <div className={skillset}>
-    <div className={title}>Skills</div>
-    <div className={skillColumns}>
-      {particularSkills.map(skillColumn)}
-    </div>
-  </div>
-</div>;
+);
 
-export default Home
+export default Home;
